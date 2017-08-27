893879dcce8e7b51ccc8be7494f8556d1dd6151f
Richard Bodai <bodaiboka@gmail.com>
2017-08-27 02:33:32 +0200

    hello sdl

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <SDL.h>
+#include <iostream>
+#undef main
+
+using namespace std;
+
+SDL_Window* pWindow = 0;
+SDL_Renderer* pRenderer = 0;
+
+int main(int argc, char* args[])
+{
+	cout << "Hello SDL 2!" << endl;
+	return 0;
+}
